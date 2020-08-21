029a8b27c004c4bffcff2b2f2ddf531bd2f2b99e
Josh Ellsworth <joshuaellsworth1@gmail.com>
2020-08-21 17:12:50 -0400

    node running

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('it works!')
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send( 'hello world' )
+});
+
+app.use(express.static("static"));
+
+app.listen(5000, function() {
+    console.log("It works!")
+});
